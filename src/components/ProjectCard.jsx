54b189c817fdce35c444a4961825844581418afd
Jon Gudenzi <jongudenzi@gmail.com>
2026-03-19 20:35:18 -0400

    Added a tags for github and app links for projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,6 +4,8 @@ export default function ProjectCard({ project }) {
         <>
             <p>{project.title}</p>
             <p>{project.description}</p>
+            <p><a href={project.github} target="_blank" rel="noreferrer">Github Repo</a></p>
+            <p><a href={project.app} target="_blank" rel="noreferrer">App</a></p>
         </>
     )
 }
