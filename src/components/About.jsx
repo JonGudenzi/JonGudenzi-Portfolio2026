592e17c6ff5638814788726265c509c5b96aaaf2
Jon Gudenzi <jongudenzi@gmail.com>
2026-03-22 23:04:37 -0400

    small edit in navbar component and added h1 header in About component so the navigation is more uniform.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ export default function About() {
     return (
         
         <div className="about-me" id="about">
+            <h1>About</h1>
             <p>I’m a web developer with 20 years of experience in inventory, logistics, IT, purchasing, and warehouse management. After reaching the top of my career in operations, I chose to pursue my long-standing interest in software development.</p>
 
             <p>I enjoy web development because it combines challenge and creativity. I focus on building solutions that make a real impact. My background has taught me to think critically, adapt quickly, and solve problems—skills I now bring into my work as a developer.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 export default function Navbar() {
 
     return (
-        <div className="navBar">
-            <a className="navText">Jon Gudenzi</a>
+        <nav className="navBar">
+            <a className="navText" href="#">Jon Gudenzi</a>
             <a className="navText" href="#about">About</a>
             <a className="navText" href="#projects">Projects</a>
             <a className="navText" href="#contact">Contact</a>
-        </div>
-
+        </nav>
     )
 }
